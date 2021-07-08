a614842e5aba010f17a4d803a2dfec8efe3ef161
Tharindu Prabath <tharindu_p@epiclanka.net>
2021-07-08 21:42:58 +0530

    error codes describe

--- lib/app/utils/error_code.dart
+++ lib/app/utils/error_code.dart
@@ -1,17 +1,17 @@
 enum ErrorCode {
-  e_1000,
-  e_1100,
-  e_1200,
-  e_1500,
-  e_1510,
-  e_1520,
-  e_1530,
-  e_1540,
-  e_1550,
-  e_1560,
-  e_1590,
-  e_1591,
-  e_1592,
-  e_2010, // Bid place failed - auction Over
-  e_2020, // Bid place failed - has new latest bid
+  e_1000, // unknown_failure
+  e_1100, // server_failure
+  e_1200, // network_failure
+  e_1500, // unknown_auth_failure
+  e_1510, // user_not_found_auth_failure
+  e_1520, // uid_already_exists_auth_failure
+  e_1530, // email_already_exists_auth_failure
+  e_1540, // insufficient_permission_auth_failure
+  e_1550, // invalid_email_auth_failure
+  e_1560, // invalid_password_auth_failure
+  e_1590, // wrong_password_auth_failure
+  e_1591, // email_not_verified_auth_failure
+  e_1592, // wrong_current_password_auth_failure
+  e_2010, // bid_place_failed_auction_over_auction_failure
+  e_2020, // bid_place_failed_has_new_latest_bid_auction_failure
 }
